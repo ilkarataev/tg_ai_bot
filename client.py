fb910cdebb11ef4b9eaace3a2610424758a418a4
Ilya Karataev <ilkarvet89@gmail.com>
2023-12-30 00:55:12 +0400

    Fix audio codec

--- client.py
+++ client.py
@@ -62,10 +62,10 @@ def watermark(input_video_path,watermark_file):
     video_with_watermark = CompositeVideoClip([video_clip, watermark])
     # watermark_file='media/output_watermark.mp4'
     # Get the audio codec of the original video
-    audio_codec = video_clip.audio.codec
+    # audio_codec = video_clip.audio.codec
 
     # Save the video with the watermark and the original audio codec
-    video_with_watermark.write_videofile('../media/output.mp4', codec='libx264', audio_codec=audio_codec)
+    video_with_watermark.write_videofile('../media/output.mp4', codec='libx264', audio_codec='libfdk_aac')
     return watermark_file
 
 def create_logger(base_url):
