6640cc26f354333a65d64d9cc3f774e34148b08b
Ilya Karataev <ilkarvet89@gmail.com>
2023-12-29 15:55:24 +0400

    logs for bug

--- bot.py
+++ bot.py
@@ -237,12 +237,12 @@ def photo_handler(message):
         elif 'preview' in userInfo[str(message.chat.id)+'_get_video_clips_names_preview']:
             # Удаляем 'preview' из строки
             name_en = userInfo[str(message.chat.id)+'_get_video_clips_names_preview'].replace('preview ', '')
+            print(message.from_user.id +' preview_clip ' + str(name_en))
             preview_url=mysqlfunc.get_video_clips_name('url','',name_en)
-            if url == None:
+            if preview_url == None:
                 bot.send_message(message.from_user.id, translations["msg_bot_error"])
                 return
             url = preview_url[0]['url']
-            print('preview_clip ' + str(name_en))
             bot.send_message(message.from_user.id, translations["msg_preview"])
             # bot.send_message(message.from_user.id, text = "<a href='"+url+"'>"+userInfo[str(message.chat.id)+'_get_video_clips_names_preview'] \
             #                  +"</a>", parse_mode='HTML')
